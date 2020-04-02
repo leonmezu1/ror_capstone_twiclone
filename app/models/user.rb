2832273f86d133cbf9136475cef8632bb794abd7
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-04-01 22:36:58 -0500

    Refactored user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,7 @@ class User < ApplicationRecord
 
   has_many :followerships, class_name: 'Following', foreign_key: :user_id, dependent: :delete_all
   has_many :inverse_followerships, class_name: 'Following', foreign_key: :follower_id, dependent: :delete_all
-  has_many :followed_users2, through: :inverse_followerships, source: :user
-  has_many :followers2, through: :followerships, source: :follower
-
-  # create template User.new(username: "Leo", full_name: "Leonardo David Mezu Lobato", password: "123456", password_confirmation: "123456", email: "leo7xs@gmail.com")
-
-  # User.new(username: "Juan", full_name: "Juan Camilo", password: "123456", password_confirmation: "123456", email: "leo8xs@gmaiil.com").save
-
-  def followers
-    followerships.map { |fr| fr.follower }.compact
-  end
-
-  def followed_users
-     inverse_followerships.map { |fr| fr.user }.compact
-  end
+  has_many :followed_users, through: :inverse_followerships, source: :user
+  has_many :followers, through: :followerships, source: :follower
 
 end
